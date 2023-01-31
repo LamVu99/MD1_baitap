7bd4aad2728978a7e601546e05cc59f3ffb29e9a
unknown <dange6801@gmail.com>
2023-01-31 17:13:34 +0700

    bt

---
+++ demo1/main2.js
@@ -0,0 +1,11 @@
+var audio = new Audio("https://www.mp3saavan.com/wp-content/uploads/2021/11/Die-For-You.mp3");
+
+audio.oncanplaythrough = function () {
+    audio.play();
+}
+
+audio.loop = true;
+
+audio.onended = function () {
+    audio.play();
+}
